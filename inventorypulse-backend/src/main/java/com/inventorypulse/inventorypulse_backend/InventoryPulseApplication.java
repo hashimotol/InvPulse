1c71cd11d867f40772f9d083a6423026a64af18c
hashimotol <112348812+hashimotol@users.noreply.github.com>
2025-11-12 16:21:57 +0100

    Setup Spring environment and pom.xml, issue 1

---
+++ inventorypulse-backend/src/main/java/com/inventorypulse/inventorypulse_backend/InventoryPulseApplication.java
@@ -0,0 +1,13 @@
+package com.inventorypulse.inventorypulse_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryPulseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryPulseApplication.class, args);
+	}
+
+}
